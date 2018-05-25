dd6aaf5235862ca4d51bbd0392c9b3e7ee6f3117
Markus Lampola <markus.lampola@opr-vakuus.fi>
2018-05-25 17:33:16 +0300

    1.4 oliot taulukkoon

--- osa1/alkeet/src/index.js
+++ osa1/alkeet/src/index.js
@@ -12,7 +12,7 @@ const Otsikko = (props) => {
 const Osa = (props) => {
     console.log("Osa", props)
     return (
-    <p key={props.kurssi.key}>{props.kurssi.osa.nimi} {props.kurssi.osa.tehtavia}</p>
+        <p key={props.osa.nimi}>{props.osa.nimi} {props.osa.tehtavia}</p>
     )
 }
 
@@ -20,52 +20,48 @@ const Sisalto = (props) => {
     console.log("Sisaltö", props)
     return (
         <div>
-            <Osa kurssi={props.osat[0]} />
-            <Osa kurssi={props.osat[1]} />
-            <Osa kurssi={props.osat[2]} />
+            <Osa osa={props.osat[0]} />
+            <Osa osa={props.osat[1]} />
+            <Osa osa={props.osat[2]} />
         </div>)
- }
+}
 
 const Yhteensa = (props) => {
     console.log("Yhteensa", props)
     return (
         <p>Yhteensä {
-            props.osat.map(kurssi => kurssi.osa.tehtavia)
-            .reduce((sum, value) => sum + value)
+            props.osat.map(osa => osa.tehtavia)
+                .reduce((sum, value) => sum + value)
         } tehtävää</p>
     )
 }
 
 const App = () => {
-  const kurssi = 'Half Stack -sovelluskehitys'
-  const osa1 = {
-    nimi: 'Reactin perusteet',
-    tehtavia: 10
-  }
-  const osa2 = {
-    nimi: 'Tiedonvälitys propseilla',
-    tehtavia: 7
-  }
-  const osa3 = {
-    nimi: 'Komponenttien tila',
-    tehtavia: 14
-  }
+    const kurssi = 'Half Stack -sovelluskehitys'
+    const osat = [
+        {
+            nimi: 'Reactin perusteet',
+            tehtavia: 10
+        },
+        {
+            nimi: 'Tiedonvälitys propseilla',
+            tehtavia: 7
+        },
+        {
+            nimi: 'Komponenttien tila',
+            tehtavia: 14
+        }]
 
-  const osat = [
-      {key: 1, osa: osa1},
-      {key: 2, osa: osa2},
-      {key: 3, osa: osa3}]
-  
-  return (
-    <div>
-        <Otsikko kurssi={kurssi} />
-        <Sisalto osat={osat} />
-        <Yhteensa osat={osat} />
-    </div>
-  )
+    return (
+        <div>
+            <Otsikko kurssi={kurssi} />
+            <Sisalto osat={osat} />
+            <Yhteensa osat={osat} />
+        </div>
+    )
 }
 
 ReactDOM.render(
-  <App />,
-  document.getElementById('root')
+    <App />,
+    document.getElementById('root')
 )
