e4a4f6d341b1366d888b343dd75b6efaa4dafe88
Markus Lampola <markus.lampola@opr-vakuus.fi>
2018-09-26 17:48:36 +0300

    2.8 puhelinluettelo osa 3

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -7,7 +7,8 @@ class App extends React.Component {
             persons: [
                 { name: 'Arto Hellas' }
             ],
-            newName: ''
+            newName: '',
+            newNumber: ''
         }
     }
 
@@ -16,23 +17,32 @@ class App extends React.Component {
         console.log('nappia painettu')
 
         if (!this.state.persons.find(person => person.name ===  this.state.newName)) {
-            const persons = this.state.persons.concat({ name: this.state.newName })
+            const persons = this.state.persons.concat({ name: this.state.newName, number: this.state.newNumber })
 
             this.setState({
                 persons: persons,
-                newName: ''
+                newName: '',
+                newNumber: ''
+    
             })
         } else {
             console.log('henkilö ' + this.state.newName + ' on jo olemassa')
             this.setState({
-                 newName: ''
+                 newName: '',
+                 newNumber: ''
+     
             })
        }
     }
 
-    handleNoteChange = (event) => {
+    handleNameChange = (event) => {
         console.log(event.target.value)
         this.setState({ newName: event.target.value })
+    }
+
+    handleNumberChange = (event) => {
+        console.log(event.target.value)
+        this.setState({ newNumber: event.target.value })
     }
 
     render() {
@@ -41,14 +51,19 @@ class App extends React.Component {
                 <h2>Puhelinluettelo</h2>
                 <form onSubmit={this.addName}>
                     <div>
-                        nimi: <input value={this.state.newName} onChange={this.handleNoteChange} />
+                        nimi: <input value={this.state.newName} onChange={this.handleNameChange} />
+                    </div>
+                    <div>
+                        numero: <input value={this.state.newNumber} onChange={this.handleNumberChange} />
                     </div>
                     <div>
                         <button type="submit">lisää</button>
                     </div>
                 </form>
                 <h2>Numerot</h2>
-                {this.state.persons.map(person => <div key={person.name}> {person.name} </div>)}
+                <table><tbody>
+                {this.state.persons.map(person => <tr key={person.name}><td>{person.name}</td><td>{person.number}</td></tr>)}
+                </tbody></table>
             </div>
         )
     }
